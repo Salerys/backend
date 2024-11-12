6ae240397332868ab2edbe9110c569140df2d675
unknown <salerysofficial@gmail.com>
2024-11-12 18:44:22 +0100

    added UserView with user profile creation details

--- api/views.py
+++ api/views.py
@@ -40,8 +40,32 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
-        # First, create the user using the serializer
         user = serializer.save()
 
-        # Now create the UserProfile for the new user
         UserProfile.objects.create(user=user)
+
+
+class EditUserView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'pk'
+
+    def get_object(self):
+        # Ensure the user is only updating their own profile
+        return self.request.user
+
+    def perform_update(self, serializer):
+        user = serializer.save()
+
+        user_profile, created = UserProfile.objects.get_or_create(user=user)
+
+        # UserProfile update
+        user_profile.bio = self.request.data.get('bio', user_profile.bio)
+        user_profile.profile_picture = self.request.data.get(
+            'profile_picture', user_profile.profile_picture
+        )
+        user_profile.save()
+
+        return user  
+
