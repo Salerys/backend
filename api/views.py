4a61cd24549ba7ef6f2ca4be796d7cfb3153d77d
unknown <salerysofficial@gmail.com>
2024-11-12 18:40:36 +0100

    began on views.py addig the class definitions starting with UserView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.contenttypes.models import ContentType
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+)
+from django.db.models import Prefetch, Q
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
+from django.middleware.csrf import get_token
+from django.http import Http404, JsonResponse
+from .serializers import (
+    UserSerializer,
+    PostSerializer,
+    CommentSerializer,
+    CustomTokenSerializer,
+    UserProfileSerializer,
+    ChangePasswordSerializer,
+)
+from .models import Post, Comment, Vote, UserProfile
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+
+def csrf_token_view(request):
+    csrf_token = get_token(request)
+    return JsonResponse({'csrfToken': csrf_token})
+
+
+class CustomTokenView(TokenObtainPairView):
+    serializer_class = CustomTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        # First, create the user using the serializer
+        user = serializer.save()
+
+        # Now create the UserProfile for the new user
+        UserProfile.objects.create(user=user)
